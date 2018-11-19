5536ffc3ded7d5e94b1e47ca420b2669f04318dd
Keyden <faikoglu.f@gmail.com>
2018-11-19 14:49:04 +0100

    Initial commit

---
+++ New Unity Project/Assets/Scripts/Währung/Goldmanager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goldmanager : MonoBehaviour
+{
+
+    public static Goldmanager goldmanager;
+    [SerializeField] private int gold;
+
+    public Text goldText;
+
+    // Use this for initialization
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddGold(int amount)
+    {
+        gold += amount;
+        UpdateUI();
+    }
+
+    public void ReduceGold(int amount)
+    {
+        gold -= amount;
+        UpdateUI();
+    }
+
+    public bool CheckGold(int amount)
+    {
+        if (amount <= gold)
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+    void UpdateUI()
+    {
+        goldText.text = "G " + gold.ToString("D");
+    }
+}
